753c5ca3ab2edd6b75c75c3b5be52f95b1dae6be
HasegawaShuhei <sh1027.dev@gmail.com>
2023-12-21 22:42:51 +0900

    Share more logic

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,15 +1,29 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 kotlin {
+
+    val ktorVersion = "2.3.5"
     sourceSets {
         commonMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+        }
+
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
         }
     }
-    
+
     androidTarget {
         compilations.all {
             kotlinOptions {
